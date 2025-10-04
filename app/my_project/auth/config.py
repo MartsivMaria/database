86564d47da3c285dc740c74a20058eed5f942020
MartsivMaria <152096237+MartsivMaria@users.noreply.github.com>
2025-10-04 23:11:13 +0300

    Update config.py

--- app/my_project/auth/config.py
+++ app/my_project/auth/config.py
@@ -6,7 +6,8 @@ os.environ['LANG'] = 'en_US.UTF-8'
 sys.dont_write_bytecode = True
 
 class Config:
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Mary123')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'ajax-db-server.mysql.database.azure.com')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'maria')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Maria123')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'ajax_systems')
+    MYSQL_PORT = 3306
